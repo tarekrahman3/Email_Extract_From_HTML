332e84419dc6fb4f175762edc6b7405e67385758
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-11-10 10:59:59 +0600

    a

--- by_puppeteer.js
+++ by_puppeteer.js
@@ -6,14 +6,13 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 async function initiate_browser () {
-  const keywords = ["machine learning in finance", "quantitative finance", "algorithmic finance"];
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  page.setViewport({ width: 1920, height: 1080 });
   return page
 };
 
 async function scrap_emails(page, url) {
-  page.setViewport({ width: 1920, height: 1080 });
   page.goto(url)
   await new Promise(r => setTimeout(r, 3000));
   var results = [];
